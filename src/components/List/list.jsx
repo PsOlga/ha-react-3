f9fe6e96ab658502a0c7abf797d389caf5578237
PsOlga <psyrniko1@gmail.com>
2024-08-15 18:49:25 +0200

    ha-react-3

---
+++ src/components/List/list.jsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import closed from "./close.png";
+import stylPeople from "./module.styles.css";
+
+function List() {
+  const [people, setPeople] = useState([
+    { id: 1, name: "Иван", age: 20 },
+    { id: 2, name: "Мария", age: 22 },
+    { id: 3, name: "Алексей", age: 21 },
+    { id: 4, name: "Марина", age: 19 },
+    { id: 5, name: "Даша", age: 23 },
+    { id: 6, name: "Глеб", age: 24 },
+    { id: 7, name: "Дима", age: 18 },
+    { id: 8, name: "Гриша", age: 20 },
+    { id: 9, name: "Серафим", age: 21 }
+  ]);
+
+  const removePerson = (id) => {
+    setPeople(people.filter((person) => person.id !== id));
+  };
+
+  return (
+    <div className={stylPeople.listPeople}>
+      <h2>Список приглашенных</h2>
+      <ul className={stylPeople.ulPeople}>
+        {people.map((person) => (
+          <li key={person.id}>
+            {person.name} (Возраст: {person.age})
+            <button onClick={() => removePerson(person.id)}>
+              <img
+                className={stylPeople.imgPeople}
+                src={closed}
+                alt="closed"
+              />
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default List;
---
+++ src/components/Ratihg.jsx
@@ -0,0 +1,48 @@
+import { useState } from "react";
+
+function Rating () {
+const [ratingValue, setRatingValue] = useState(0);
+
+const handleRatingClick = (value) => {
+  setRatingValue(value);
+  };
+
+ const ratingList = [
+    // '/imgs/stars_24.png',
+    // './imgs/stars_2.png',
+    // './imgs/stars_3.png',
+    // './imgs/stars_4.png',
+  ];
+
+
+const style = {
+    backgroundColor: 'rgb(204, 243, 243)', 
+    borderRadius: '5px', 
+    margin:'15px',
+    border: 'solid 1px rgb(79, 122, 122)',
+    color: 'rgb(55, 88, 88)',
+    padding: '5px 10px',
+  
+}
+return (
+ <div style={{backgroundColor: 'rgb(204, 243, 243)', }}>
+<div className="ratingDiv">
+{ratingValue > 0 && (
+    < img src={ratingList[ratingValue - 1]}
+    aria-colcount={'Rating ${ratingValue}'}
+    style={{width: '100px', height: 'auto'}}
+    />
+)}
+  </div>
+  <div className="btnsDiv" >
+    <button onClick={() => handleRatingClick(1)} style={style}>Плохо</button>
+    <button onClick={() => handleRatingClick(2)}style={style}>Приемлемо</button>
+    <button onClick={() => handleRatingClick(3)} style={style}>Хорошо</button>
+    <button onClick={() => handleRatingClick(4)} style={style}>Отлично</button>
+  </div>
+
+
+</div>
+)
+}
+export default Rating;
